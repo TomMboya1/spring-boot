c6ad956df5f271d0d74917060a08c214977ec543
Tom mboya <tommboya044@gmail.com>
2023-10-09 11:47:15 +0300

    2ns commit

--- src/main/java/com/example/springboot/controller/IngredientController.java
+++ src/main/java/com/example/springboot/controller/IngredientController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class IngredientController {
     private IngredientService ingredientService;
     private MealService mealService;
+    private List<Meal> meals = new ArrayList<>();
 
     @Autowired
     public IngredientController(IngredientService ingredientService, MealService mealService) {
@@ -26,4 +28,30 @@ public class IngredientController {
 
         return ResponseEntity.ok().build();
     }
+    @PostMapping("/meal")
+    public void addMeal(@RequestBody Meal meal) {
+        meals.add(meal);
+    }
+
+    @PutMapping("/meal/{name}")
+    public void updateMeal(@PathVariable String name, @RequestBody Meal updatedMeal) {
+        for (Meal meal : meals) {
+            if (meal.getName().equals(name)) {
+                meal.setName(updatedMeal.getName());
+                meal.setDescription(updatedMeal.getDescription());
+                meal.setPrice(updatedMeal.getPrice());
+                break;
+            }
+        }
+    }
+
+    @DeleteMapping("/meal/{name}")
+    public void deleteMeal(@PathVariable String name) {
+        meals.removeIf(meal -> meal.getName().equals(name));
+    }
+    @DeleteMapping("/meal/price/{price}")
+    public void deleteMealsAbovePrice(@PathVariable double price) {
+        meals.removeIf(meal -> meal.getPrice() > price);
+    }
+
 }
