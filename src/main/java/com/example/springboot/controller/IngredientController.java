8160ea7bfd259d7f6dc402aabfacc0e5068b52d8
Tom mboya <tommboya044@gmail.com>
2023-10-09 12:30:39 +0300

    2ns commit

--- src/main/java/com/example/springboot/controller/IngredientController.java
+++ src/main/java/com/example/springboot/controller/IngredientController.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class IngredientController {
     private IngredientService ingredientService;
     private MealService mealService;
-    private List<Meal> meals = new ArrayList<>();
+
 
     @Autowired
     public IngredientController(IngredientService ingredientService, MealService mealService) {
@@ -28,40 +28,6 @@ public class IngredientController {
 
         return ResponseEntity.ok().build();
     }
-    @PostMapping("/meal")
-    public void addMeal(@RequestBody Meal meal) {
-        meals.add(meal);
-    }
 
-    @PutMapping("/meal/{name}")
-    public void updateMeal(@PathVariable String name, @RequestBody Meal updatedMeal) {
-        for (Meal meal : meals) {
-            if (meal.getName().equals(name)) {
-                meal.setName(updatedMeal.getName());
-                meal.setDescription(updatedMeal.getDescription());
-                meal.setPrice(updatedMeal.getPrice());
-                break;
-            }
-        }
-    }
-
-    @DeleteMapping("/meal/{name}")
-    public void deleteMeal(@PathVariable String name) {
-        meals.removeIf(meal -> meal.getName().equals(name));
-    }
-    @DeleteMapping("/meal/price/{price}")
-    public void deleteMealsAbovePrice(@PathVariable double price) {
-        meals.removeIf(meal -> meal.getPrice() > price);
-    }
-
-    @PutMapping("/meal/{name}/price")
-    public void updateMealPrice(@PathVariable String name, @RequestBody double updatedPrice) {
-        for (Meal meal : meals) {
-            if (meal.getName().equals(name)) {
-                meal.setPrice(updatedPrice);
-                break;
-            }
-        }
-    }
 
 }
--- src/main/java/com/example/springboot/controller/MealController.java
+++ src/main/java/com/example/springboot/controller/MealController.java
@@ -1,20 +1,20 @@
 package com.example.springboot.controller;
 
-import com.example.springboot.model.Ingredient;
 import com.example.springboot.model.Meal;
 import com.example.springboot.service.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/meal")
 public class MealController {
+    private List<Meal> meals = new ArrayList<>();
     private MealService mealService;
+    private Meal meal;
 
     @Autowired
     public MealController(MealService mealService) {
@@ -26,11 +26,11 @@ public class MealController {
         return ResponseEntity.ok(mealService.getMeals());
     }
 
-    @PutMapping("/meal")
-    public ResponseEntity<Meal> addMeal(@RequestBody Meal meal) {
-        mealService.addMeal(meal);
-        return ResponseEntity.ok(meal);
-    }
+//    @PutMapping("/meal")
+//    public ResponseEntity<Meal> addMeal(@RequestBody Meal meal) {
+//        mealService.addMeal(meal);
+//        return ResponseEntity.ok(meal);
+//    }
 
     @DeleteMapping("/meal/{id}")
     public ResponseEntity<Meal> deleteMeal(@PathVariable long id) {
@@ -38,11 +38,11 @@ public class MealController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/meal")
-    public ResponseEntity<Meal> updateMeal(@RequestBody Meal meal) {
-        mealService.updateMeal(meal);
-        return ResponseEntity.ok(meal);
-    }
+//    @PostMapping("/meal")
+//    public ResponseEntity<Meal> updateMeal(@RequestBody Meal meal) {
+//        mealService.updateMeal(meal);
+//        return ResponseEntity.ok(meal);
+//    }
 
     @GetMapping("/summer-meals")
     public ResponseEntity<List<Meal>> getSummerMeals() {
@@ -52,6 +52,43 @@ public class MealController {
     @GetMapping("/secret-formula")
     public ResponseEntity<String> getSecretFormula() {
         return ResponseEntity.ok("2 + 2 = 4");
+    }
+    @PostMapping("/meal")
+    public void addMeal(@RequestBody Meal meal) {
+        meals.add(meal);
+    }
+    @PutMapping("/meal/{name}")
+    public void updateMeal(@PathVariable String name, @RequestBody Meal updatedMeal) {
+        for (Meal meal : meals) {
+            if (meal.getName().equals(name)) {
+                meal.setName(updatedMeal.getName());
+                meal.setDescription(updatedMeal.getDescription());
+                meal.setPrice(updatedMeal.getPrice());
+                break;
+            }
+        }
+    }
+    @DeleteMapping("/meal/{name}")
+    public void deleteMeal(@PathVariable String name) {
+        meals.removeIf(meal -> meal.getName().equals(name));
+    }
+
+
+
+
+    @DeleteMapping("/meal/price/{price}")
+    public void deleteMealsAbovePrice(@PathVariable double price) {
+        meals.removeIf(meal -> meal.getPrice() > price);
+    }
+
+    @PutMapping("/meal/{name}/price")
+    public void updateMealPrice(@PathVariable String name, @RequestBody double updatedPrice) {
+        for (Meal meal : meals) {
+            if (meal.getName().equals(name)) {
+                meal.setPrice(updatedPrice);
+                break;
+            }
+        }
     }
 
 }
